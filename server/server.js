6b629ddb4c4f4ce77a14db736509ab5abb9dd6d0
Leila Skinner <beileiwang@gmail.com>
2020-04-03 16:22:31 -0500

    server.js files setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const taskRouter = require("./routes/task.router");
 
 const PORT = 5000;
 
@@ -9,6 +10,9 @@ app.use(bodyParser.json());
 
 // serve static file
 app.use(express.static("server/public"));
+
+//configure routes
+app.use("/tasks", taskRouter);
 
 // runs the server
 app.listen(PORT, () => {
