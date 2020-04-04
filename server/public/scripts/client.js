aa91d2151b18ccc842ffe0c72f2392aac5e535b9
Leila Skinner <beileiwang@gmail.com>
2020-04-04 12:38:17 -0500

    post ajax call created and add new task from frontend working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,15 +2,33 @@ $(document).ready(init);
 
 function init() {
   console.log("DOM is ready!");
+  // add event listener for add task button
+  $(".js-add-task-btn").on("click", addTask);
+  // add event listener for complete button
+
+  // add event listener for delete button
 
   // get all tasks when page loads
-  getTasks();
+  getTasksFromServer();
 }
 
 // EVENT HANDLER
 
+function addTask() {
+  const newTaskObject = {
+    task: $(".js-input-new-task").val()
+  };
+  sendTaskToServer(newTaskObject);
+}
+
+function completeTask() {}
+
+function deleteTask() {}
+
+// API INTERACTIONS
+
 // get tasks
-function getTasks() {
+function getTasksFromServer() {
   $.ajax({
     method: "GET",
     url: "/tasks"
@@ -23,7 +41,27 @@ function getTasks() {
     });
 }
 
-// API INTERACTIONS
+// post task
+function sendTaskToServer(taskObject) {
+  $.ajax({
+    method: "POST",
+    url: "/tasks",
+    data: taskObject
+  })
+    .then(response => {
+      console.log(`POST TASK: ${response}`);
+      getTasksFromServer();
+    })
+    .catch(error => {
+      console.log(`POST ERROR ${error}`);
+    });
+}
+
+// put/complete task
+function sendCompetedTaskToServer() {}
+
+// delete task
+function sendDeleteTaskToServer() {}
 
 // RENDER TO DOM
 function renderAllTasks(taskArray) {
