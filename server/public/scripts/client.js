11d848a0fa9e1c5f0321852305355f0d4ab0bb7a
Leila Skinner <beileiwang@gmail.com>
2020-04-04 12:59:05 -0500

    put ajax call created and update task from frontend working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,7 +5,7 @@ function init() {
   // add event listener for add task button
   $(".js-add-task-btn").on("click", addTask);
   // add event listener for complete button
-
+  $(".js-todo-list").on("click", ".js-btn-complete", completeTask);
   // add event listener for delete button
 
   // get all tasks when page loads
@@ -21,7 +21,15 @@ function addTask() {
   sendTaskToServer(newTaskObject);
 }
 
-function completeTask() {}
+function completeTask(event) {
+  const completedTaskId = event.target.dataset.id;
+  console.log(`Completed Task Id: ${completedTaskId}`);
+  const completedTaskObject = {
+    id: completedTaskId,
+    completed: "true"
+  };
+  sendCompetedTaskToServer(completedTaskObject);
+}
 
 function deleteTask() {}
 
@@ -58,17 +66,30 @@ function sendTaskToServer(taskObject) {
 }
 
 // put/complete task
-function sendCompetedTaskToServer() {}
+function sendCompetedTaskToServer(taskObject) {
+  $.ajax({
+    method: "PUT",
+    url: `/tasks/${taskObject.id}`,
+    data: taskObject
+  })
+    .then(response => {
+      console.log(`PUT TASK: ${response}`);
+      getTasksFromServer();
+    })
+    .catch(error => {
+      console.log(`PUT ERROR ${error}`);
+    });
+}
 
 // delete task
-function sendDeleteTaskToServer() {}
+function sendDeleteTaskToServer(taskObject) {}
 
 // RENDER TO DOM
 function renderAllTasks(taskArray) {
   $(".js-todo-list").empty();
   for (let task of taskArray) {
     $(".js-todo-list").append(`
-        <li>${task.task} <button class"js-btn-complete">Complete</button> <button class"js-btn-delete">Delete</button></li>
+        <li>${task.task} <button class="js-btn-complete" data-id="${task.id}">Complete</button> <button class="js-btn-delete" data-id="${task.id}">Delete</button></li>
         `);
   }
 }
